38e9c4601f7185887efb196d27fa5bb71ca5d7f8
Karl Vanderslice <karl@karlvanderslice.com>
2024-12-18 08:53:49 -0500

    chore: reverting

--- tools/check_pywayland.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import pywayland
-import pkgutil
-
-print(f"PyWayland version: {pywayland.__version__}")
-print("\nAvailable modules:")
-for importer, modname, ispkg in pkgutil.walk_packages(pywayland.__path__, pywayland.__name__ + '.'):
-    print(modname)
